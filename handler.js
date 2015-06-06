d42ed65e936fece11860e3f2a58b299b6f024fd1
lizhuoli <lizhuoli1126@126.com>
2015-06-06 20:23:46 +0800

    f

--- handler.js
+++ handler.js
@@ -61,7 +61,7 @@ wechat.on('image', function(session) {
         return;
       }
 
-      var imageBase64 = new Buffer(res.body, 'base64').toString();
+      var imageBase64 = new Buffer(res.body, 'base64').toString('utf8');
 
       var preBullet = {
       	type: 'image',
