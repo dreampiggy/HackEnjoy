844773b5063fc65563075d7e322e187590589e4f
lizhuoli <lizhuoli1126@126.com>
2015-06-07 06:01:31 +0800

    1

--- handler.js
+++ handler.js
@@ -239,7 +239,7 @@ function checkBullet (results){
 			return random(3000,4000);
 		}
 	})();
-	var fontsize = (duration / 1000) - 2;
+	var fontsize = (random(3000,5000) / 1000) - 2;
 
 	bullet.type = type;
 	bullet.color = color;
